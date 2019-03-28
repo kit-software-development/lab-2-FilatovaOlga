1d2777788a84bdd4e5f39d83daff02bb57f3d617
FilatovaOlga <47888568+FilatovaOlga@users.noreply.github.com>
2019-03-28 16:46:39 +0300

    Commit

--- Implementation/Common/Name.cs
+++ Implementation/Common/Name.cs
@@ -3,11 +3,34 @@
     /// <summary>
     ///     Скрытая реализация представления об имени человека.
     /// </summary>
-    internal struct Name
+    internal struct Name : IName
     {
         /*
          * TODO #1: Реализуйте интерфейс IName для структуры Name
          */
+
+        public string FullName { get; }
+
+        public string ShortName { get; }
+
+        public Name(string surname, string firstname, string patronymic)
+        {
+            FirstName = firstname;
+            Surname = surname;
+            Patronymic = patronymic;
+            FullName = surname + ' ' + firstname + ' ' + patronymic;
+            ShortName = surname + ' ' + firstname[0] + "." + patronymic[0] + '.';
+        }
+
+        public Name(IName name)
+        {
+            FullName = name.FullName;
+            ShortName = name.ShortName;
+            var mas = FullName.Split(' ');
+            Surname = mas[0];
+            FirstName = mas[1];
+            Patronymic = mas[2];
+        }
 
         /// <summary>
         ///     Имя.
